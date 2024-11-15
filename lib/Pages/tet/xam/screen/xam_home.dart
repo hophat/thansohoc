744d7cb2f41ac69b1b8324f757c633565deb9f3b
Duong Dang <duongplus1011@gmail.com>
2024-11-15 17:31:24 +0700

    add -> screen shot

--- lib/Pages/tet/xam/screen/xam_home.dart
+++ lib/Pages/tet/xam/screen/xam_home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 // import 'package:gieoque/const.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:screenshot/screenshot.dart';
 
 import '../widget/result.dart';
 import '../widget/tet_background.dart';
@@ -18,6 +20,7 @@ class _XamHomeState extends State<XamHome> {
   bool result = false;
   bool isShakingInstruct = true;
   final txtColor = const Color(0xFFFFF385);
+  ScreenshotController _screenshotController = ScreenshotController();
   @override
   void initState() {
     super.initState();
@@ -29,69 +32,89 @@ class _XamHomeState extends State<XamHome> {
     // isShakingInstruct = globalShared.getBool(instructionKey) ?? true;
     // isShakingInstruct = true;
   }
-
+  Uint8List? _imgBytes;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          const Positioned.fill(child: TetBackground()),
-          AnimatedSwitcher(
-            duration: const Duration(milliseconds: 800),
-            child: isShakingInstruct
-                ? Align(
-                    key: const ValueKey('instruction'),
-                    alignment: Alignment.center,
-                    child: Container(
-                      alignment: Alignment.center,
-                      decoration: const BoxDecoration(
-                        color: Colors.black87,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Lottie.asset('assets/shaking_phone.json'),
-                          Text('Lắc mạnh phone để lấy quẻ', style: GoogleFonts.livvic(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: txtColor,
-                          ),),
-                          const SizedBox(height: 16),
-                          _submit(),
-                          // ElevatedButton(
-                          //   child: const Text('Bắt đầu'),
-                          //   onPressed: () {
-                          //     setState(() {
-                          //       isShakingInstruct = false;
-                          //       globalShared.setBool(instructionKey, isShakingInstruct);
-                          //     });
-                          //   },
-                          // ),
+    return Screenshot(
+      controller: _screenshotController,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          _screenshotController.capture().then((capturedImage) {
+            _imgBytes = capturedImage;
+            if (capturedImage != null) {
+              print('_screenshotController Capture Done');
+              setState(() {
 
-                        ],
+              });
+            }
+          }).catchError((onError) {
+            print('_screenshotController Capture Error');
+          });
+        }),
+        body: Stack(
+          children: [
+            const Positioned.fill(child: TetBackground()),
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 800),
+              child: isShakingInstruct
+                  ? Align(
+                      key: const ValueKey('instruction'),
+                      alignment: Alignment.center,
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(
+                          color: Colors.black87,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Lottie.asset('assets/shaking_phone.json'),
+                            Text('Lắc mạnh phone để lấy quẻ', style: GoogleFonts.livvic(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: txtColor,
+                            ),),
+                            const SizedBox(height: 16),
+                            _submit(),
+                            // ElevatedButton(
+                            //   child: const Text('Bắt đầu'),
+                            //   onPressed: () {
+                            //     setState(() {
+                            //       isShakingInstruct = false;
+                            //       globalShared.setBool(instructionKey, isShakingInstruct);
+                            //     });
+                            //   },
+                            // ),
+      
+                          ],
+                        ),
                       ),
+                    )
+                  : Align(
+                      key: const ValueKey('xam'),
+                      alignment: Alignment.center,
+                      child: !result
+                          ? Xam(
+                              onResult: () {
+                                result = true;
+                                setState(() {});
+                              },
+                            )
+                          : Result(
+                              onRetry: () {
+                                result = false;
+                                setState(() {});
+                              },
+                            ),
                     ),
-                  )
-                : Align(
-                    key: const ValueKey('xam'),
-                    alignment: Alignment.center,
-                    child: !result
-                        ? Xam(
-                            onResult: () {
-                              result = true;
-                              setState(() {});
-                            },
-                          )
-                        : Result(
-                            onRetry: () {
-                              result = false;
-                              setState(() {});
-                            },
-                          ),
-                  ),
-          ),
-        ],
+            ),
+
+            // _imgBytes != null ? Positioned.fill(
+            //   child: Image.memory(_imgBytes!, fit: BoxFit.cover,),
+            // ) : const SizedBox.shrink(),
+          ],
+        ),
       ),
     );
   }
